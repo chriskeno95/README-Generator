198f16658bfb8df17d95e2178c9e29791612ccb4
chris kennedy <chris-kennedy09@hotmail.com>
2023-02-24 23:03:18 +0000

    added questions and ran a test to confim questions are displaying in node environment

---
+++ README_Template.md
@@ -0,0 +1,3 @@
+# test
+  ![Github licence](https://img.shields.il/badge/license-MIT-blue.svg)
+
--- index.js
+++ index.js
@@ -8,54 +8,67 @@ const questions = [
         {
             type: 'input',
             name: 'licence',
-            message: ""
+            message: "What kind of Licence does this project have?",
+            choices: ["MIT","APACHE2.0","GPL","None"]
         },
         {
             type: 'input',
             name: 'title',
-            message: ""
+            message: "What is the name for this project?"
         },
         {
             type: 'input',
             name: 'description',
-            message: ""
+            message: "Write a short description for this project"
         },
         {
             type: 'input',
             name: 'installation',
-            message: ""
+            message: "What command should be used to install dependancies?",
+            default: "npm i"
         },
         {
             type: 'input',
             name: 'test',
-            message: ""
+            message: "What command should be used to run a test",
+            default: "npm test"
         },
         {
             type: 'input',
             name: 'usage',
-            message: ""
+            message: "What does the user need to know about using this repository"
         },
         {
             type: 'input',
             name: 'contributing',
-            message: ""
+            message: "What does the user need to know about contributing to this repository"
         },
         {
             type: 'input',
             name: 'email',
-            message: ""
+            message: "What is your email address?"
+        },
+        {
+            type: 'input',
+            name: 'Github',
+            message: "What is your Github username?"
         },
         
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    //returns current working directory of the node.js process
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((response) => {
+    console.log(`Generating README...`);
+    writeToFile(`README_Template.md`, generateMarkdown({...response}))
 }
+)}
 
 // function call to initialize program
 init();
